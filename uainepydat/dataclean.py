742eaa8193ba3f67e16be621ccb0d7e86912a848
Daniel <39141263+uaineteine@users.noreply.github.com>
2025-02-12 01:09:11 +1100

    import sig

--- uainepydat/dataclean.py
+++ uainepydat/dataclean.py
@@ -1,3 +1,5 @@
+from pandas import DataFrame
+
 def clean_whitespace_in_df(df: DataFrame) -> DataFrame:
     df_cleaned = df.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
     return df_cleaned
