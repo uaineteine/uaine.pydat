cf1e70356be75c69c52997a6a8f3fc35c4e195e8
Daniel <39141263+uaineteine@users.noreply.github.com>
2025-02-12 01:07:48 +1100

    added signature

--- uainepydat/dataclean.py
+++ uainepydat/dataclean.py
@@ -1,3 +1,3 @@
-def clean_whitespace_in_df(df):
+def clean_whitespace_in_df(df: DataFrame) -> DataFrame:
     df_cleaned = df.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
     return df_cleaned
