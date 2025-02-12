9841b5370ed23a50e8d7c2ead76b59a6334a1084
Daniel Stamer-Squair <uaine.teine@hotmail.com>
2025-02-12 23:15:25 +1100

    start of notes build with autofunctions

---
+++ docs/source/conf.py
@@ -0,0 +1,36 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import sys
+from pathlib import Path
+
+auto_libs_loc = str(Path('..', '..', "uainepydat").resolve())
+sys.path.insert(0,auto_libs_loc)
+
+project = 'Uaine.Pydat'
+copyright = '2025, Daniel Stamer-Squair'
+author = 'Daniel Stamer-Squair'
+release = '2025'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+extensions = [
+    'sphinx.ext.autodoc',
+]
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ docs/source/index.rst
@@ -0,0 +1,18 @@
+.. Uaine.Pydat documentation master file, created by
+   sphinx-quickstart on Wed Feb 12 22:51:18 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Uaine.Pydat documentation
+=========================
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+.. autofunction:: dataclean.clean_whitespace_in_df
