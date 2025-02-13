4641dfbe116ff68165f453cf46f7c3aab730f0cf
Daniel Stamer-Squair <uaine.teine@hotmail.com>
2025-02-14 00:20:53 +1100

    update signatures, new method for replacing tags

--- uainepydat/dataclean.py
+++ uainepydat/dataclean.py
@@ -1,5 +1,20 @@
 from pandas import DataFrame
+from typing import List
 
 def clean_whitespace_in_df(df: DataFrame) -> DataFrame:
     df_cleaned = df.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
     return df_cleaned
+
+def replace_between_tags(content: str, tag_name: str, new_lines: List[str]) -> str:
+    start_tag = f'<{tag_name}>'
+    end_tag = f'</{tag_name}>'
+    
+    start_index = content.find(start_tag)
+    end_index = content.find(end_tag, start_index)
+
+    if start_index == -1 or end_index == -1:
+        raise ValueError("Tags not found in the content")
+
+    new_content = content[:start_index + len(start_tag)] + '\n' + '\n'.join(new_lines) + '\n' + content[end_index:]
+
+    return new_content
