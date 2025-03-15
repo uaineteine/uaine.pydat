e6617d6c72e7d8c5ff05a6f91b942a905c8eb920
Daniel Stamer-Squair <uaine.teine@hotmail.com>
2025-03-15 16:14:04 +1100

    fix licence ref

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     packages=["uainepydat"],
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.9',
